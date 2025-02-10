600baba80cd56fd6277e8d3d1f6737435b996a0b
linusmtz <linusmartinez00@gmail.com>
2025-02-10 13:38:39 -0600

    Clase 10 febrero

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,18 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
 
@@ -25,30 +23,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Content()
-                }
+                Content("Hola", "Mundo")
             }
         }
     }
 }
 
+@Composable
+fun Content(mensaje1: String, mensaje2: String) {
+    Column {
+        Text(mensaje1,fontSize=30.sp)
+        Text(mensaje2, lineHeight = 30.sp)
+        Text("Luis", fontWeight = FontWeight.Bold, fontSize = 30.sp, lineHeight = 32.sp)
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun Content(){
-    Text(
-        text = "Hola Jetpack Compose",
-        color = Color.Red,
-        fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier.padding().background(Color.Blue)
-    )
-
-    /*
-    * De posicionamiento = width, height, top, bottom, start, end
-    * De funcionalidad = click,scroll
-    * De apariencia = background, padding, border
-    * */
+fun GreetingPreview() {
+    Content("Hola", "Mundo")
 }
